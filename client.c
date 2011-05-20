bc15cc16f5a74bd430723a2d64c00f947856c687
Marc <marc.bertram@googlemail.com>
2011-05-20 13:06:19 +0200

    Connect works

--- client.c
+++ client.c
@@ -4,8 +4,12 @@
         Author: Marc Bertram
 */
 
+#include <stdio.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+
 
 /* ------------------------------ */
 
@@ -15,7 +19,7 @@ int main (int argc, char *argv[])
   int s;
   struct sockaddr_in addr;
 
-  addr.sin_addr.s_addr = inet_addr("78.47.146.248");    /* z.B. inet_addr("127.0.0.1"); */
+  addr.sin_addr.s_addr = inet_addr("127.0.0.1");    /* z.B. inet_addr("127.0.0.1"); */
   addr.sin_port = htons(5001);             /* z.B. htons(80);              */
   addr.sin_family = AF_INET;
 
@@ -28,7 +32,7 @@ int main (int argc, char *argv[])
   }
 
   // Connect to Server
-  if (connect(s, &addr, sizeof(addr)) == -1)
+  if (connect(s, (struct sockaddr *)&addr, sizeof(addr)) == -1)
   {
     perror("connect() failed");
     return 2;
--- server.c
+++ server.c
@@ -4,14 +4,31 @@
         Author: Marc Bertram
 */
 
+#include <stdio.h>
+
+
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 /* ------------------------------ */
 
+void client_behandlung(int client)
+{
+}
 
+/* ------------------------------ */
 int main (int argc, char *argv[])
 {
+  int s, c;
+  struct sockaddr_in addr;
+  struct sockaddr_in cli;
+  int cli_size;
+
+
+  addr.sin_addr.s_addr = INADDR_ANY;	/* z.B. inet_addr("127.0.0.1"); */
+  addr.sin_port = htons(5001); 		/* z.B. htons(80);              */
+  addr.sin_family = AF_INET;
 
   // Create Socket
   s = socket(AF_INET, SOCK_STREAM, 0);
@@ -21,7 +38,30 @@ int main (int argc, char *argv[])
     return 1;
   }
 
-  
+  // Bind Socket
+  if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+  {
+    perror("bind() failed");
+    return 2;
+  }
+
+  // Listen
+  if (listen(s, 3) == -1)
+  {
+    perror("listen () failed");
+    return 3;
+  }
+
+  // Accept Client Connection
+  cli_size = sizeof(cli);
+  //c = accept(s, &cli, &cli_size);
+  for(;;)
+  {
+    c = accept(s, (struct sockaddr *)&cli, &cli_size);
+    printf("Verbindung von %s\n", inet_ntoa(cli.sin_addr));
+    client_behandlung(c);
+    close(c);
+  }
 
   close();
 

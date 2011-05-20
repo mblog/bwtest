f74332b8d20a5959cd77bde157845f6429e336ff
Marc <marc.bertram@googlemail.com>
2011-05-20 12:00:22 +0200

    Client can connect to Server

--- client.c
+++ client.c
@@ -12,6 +12,12 @@
 
 int main (int argc, char *argv[])
 {
+  int s;
+  struct sockaddr_in addr;
+
+  addr.sin_addr.s_addr = inet_addr("78.47.146.248");    /* z.B. inet_addr("127.0.0.1"); */
+  addr.sin_port = htons(5001);             /* z.B. htons(80);              */
+  addr.sin_family = AF_INET;
 
   // Create Socket
   s = socket(AF_INET, SOCK_STREAM, 0);
@@ -21,7 +27,15 @@ int main (int argc, char *argv[])
     return 1;
   }
 
+  // Connect to Server
+  if (connect(s, &addr, sizeof(addr)) == -1)
+  {
+    perror("connect() failed");
+    return 2;
+  }
+
   
+
 
   close();
 
